45833408d62816e95f200d99912963fec6c553b1
taufiq72 <taufiqq1207@gmail.com>
2026-05-12 10:57:00 +0700

    membuat else pada peek

--- StackLinkedList.cpp
+++ StackLinkedList.cpp
@@ -55,5 +55,15 @@ public:
         {
             cout << "List is Empty" << endl;
         }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
     }
 };
