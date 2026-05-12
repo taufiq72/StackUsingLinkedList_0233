028793ef506ac6990480bacf6be42a89d407feca
taufiq72 <taufiqq1207@gmail.com>
2026-05-12 10:53:43 +0700

    melengkapi void pop

--- StackLinkedList.cpp
+++ StackLinkedList.cpp
@@ -42,5 +42,9 @@ public:
         {
             cout << "Stack is Empty" << endl;
         }
+
+        Node *temp = top;
+        top = top->next;
+        cout << "Popped value : " << top->data << endl;
     }
 };
