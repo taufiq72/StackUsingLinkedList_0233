c2689e8ef77838fc08261dcbffbd4aa792276bde
taufiq72 <taufiqq1207@gmail.com>
2026-05-12 11:03:51 +0700

    default

--- StackLinkedList.cpp
+++ StackLinkedList.cpp
@@ -119,6 +119,11 @@ int main()
         case 4:
             cout << "Exiting program" << endl;
             break;
+        default:
+            cout << "Invalid choice, try again" << endl;
         }
-    }    
+    } while (choice != 4);
+
+    cout << endl;
+    return 0;
 };
