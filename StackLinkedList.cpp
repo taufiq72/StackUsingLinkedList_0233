e53afd4fc4284f1f9e15975b7b67fed6d7efcb48
taufiq72 <taufiqq1207@gmail.com>
2026-05-12 10:51:34 +0700

    membuat int push

--- StackLinkedList.cpp
+++ StackLinkedList.cpp
@@ -25,4 +25,14 @@ public:
     {
         top = NULL;
     }
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push Value" << value << endl;
+        return value;
+    }
+
 };
